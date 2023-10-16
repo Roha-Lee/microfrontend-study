4f259582279aa3c1c0d974593ff1ca00cc12ec50
Roha-Lee <roha.lee@buzzvil.com>
2023-10-16 16:47:22 +0900

    use share to remove unnecessary fetch

--- example1/cart/webpack.config.js
+++ example1/cart/webpack.config.js
@@ -4,19 +4,19 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 module.exports = {
     mode: "development",
     devServer: {
-        port: 8081,
+        port: 8082,
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: "products",
+            name: "cart",
             filename: "remoteEntry.js",
             exposes: {
-                "./ProductsIndex": "./src/index"
+                "./CartShow": "./src/index"
             },
             shared: ["faker"],
         }),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
         }),
-    ]
+    ],
 }
